666d40b6c3cad7347ae0046cc361341f76a8be28
jewelrana <tonujewel@gmail.com>
2024-05-16 00:44:51 +0600

    [Add] Network client

---
+++ lib/core/networking/api_url.dart
@@ -0,0 +1,7 @@
+class ApiUrl {
+  // Base url
+  static const baseUrl = "https://api.yippeekart.com.bd/api/v1";
+
+  // Product list 
+  static String productListUrl = "$baseUrl/public/new-arrival/products";
+}
