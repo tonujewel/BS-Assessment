c41395e98a880ddcdae2ea8b38af86ac413fed55
jewelrana <tonujewel@gmail.com>
2024-05-17 10:56:19 +0600

    [update] base url changes

--- lib/core/networking/api_url.dart
+++ lib/core/networking/api_url.dart
@@ -1,7 +1,8 @@
 class UrlManager {
   // Base url
-  static const baseUrl = "https://api.yippeekart.com.bd/api/v1";
+  static const baseUrl = "https://api.github.com/";
 
   // Product list
   static String productListUrl = "$baseUrl/public/new-arrival/products";
+  static String searchUrl = "$baseUrl/search/repositories?q=Flutter&per_page=10&sort=stars";
 }
